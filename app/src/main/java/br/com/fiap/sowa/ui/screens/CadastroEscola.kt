1ed019e6cbcf87ab71f838aa2497ff10fc9d1f33
Joao-Roberto <jrrdasilva@gmail.com>
2024-03-17 19:43:52 -0300

    Tela CadastroEscola com botão Escola

--- app/src/main/java/br/com/fiap/sowa/ui/screens/CadastroEscola.kt
+++ app/src/main/java/br/com/fiap/sowa/ui/screens/CadastroEscola.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
@@ -43,11 +44,89 @@ fun CadastroEscola(navController: NavController) {
             Spacer(modifier = Modifier.height(16.dp))
             LoginAndCadastro(navController)
             Spacer(modifier = Modifier.height(16.dp))
+            EscolaOrProfissional(navController)
+            Spacer(modifier = Modifier.height(16.dp))
             Spacer(modifier = Modifier.height(16.dp))
             Spacer(modifier = Modifier.weight(1f))
         }
     }
 }
+
+@Composable
+fun EscolaOrProfissional(navController: NavController){
+    var email by remember { mutableStateOf("") }
+    var senha by remember { mutableStateOf("") }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(horizontal = 64.dp)
+    )
+
+    {
+
+        Text(
+            text = "Você é um(a)",
+            fontSize = 15.sp,
+            color = Color.Black,
+            textAlign = TextAlign.Left,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = { /* Ação ao clicar no botão */ },
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.bluePrincipal)),
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier
+                .align(Alignment.End)
+                .width(100.dp)
+                .height(35.dp)
+        ) {
+            Text(
+                text = "Escola",
+                color = Color.White,
+                fontSize = 12.sp,
+            )
+        }
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { letra ->
+                email = letra
+            },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            label = { Text(text = "Email") },
+            placeholder = { Text(text = "Digite seu email") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+
+        Spacer(modifier = Modifier.height(7.dp))
+
+        OutlinedTextField(
+            value = senha.replace(Regex("."), "*"),
+            onValueChange = { letra ->
+                senha = letra
+            },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            label = { Text(text = "Senha") },
+            placeholder = { Text(text = "Digite sua senha") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.eye),
+                    contentDescription = "Ícone de olho.",
+                    modifier = Modifier.size(20.dp)
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+
+        }
+    }
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
